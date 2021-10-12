247b70eb97cd7cb08e89f02115501d90bc620897
Jared <jared.orr1209@gmail.com>
2021-10-11 22:00:19 -0400

    End of seciton 11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -27,8 +27,8 @@ export class MembersService {
   }
 
   getMember(username: string) {
-    const member = this.members.find(x => x.username === username);
-    if(member !== undefined){
+    const member = this.members.find((x) => x.username === username);
+    if (member !== undefined) {
       return of(member);
     }
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
@@ -40,6 +40,14 @@ export class MembersService {
         const index = this.members.indexOf(member);
         this.members[index] = member;
       })
-    )
+    );
+  }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 }
