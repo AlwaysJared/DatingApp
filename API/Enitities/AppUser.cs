b7f6111567437d19da17287131738436acfac6ac
Jared <jared.orr1209@gmail.com>
2021-12-01 13:13:28 -0500

    End of section 16

--- API/Enitities/AppUser.cs
+++ API/Enitities/AppUser.cs
@@ -1,18 +1,20 @@
 using System;
 using System.Collections.Generic;
-using API.Extensions;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Enitities
 {
-    public class AppUser
+    public class AppUser : IdentityUser<int>
     {
-        public int Id { get; set; }
+        // removed when refactoring to .net identity framework
 
-        public string UserName { get; set; }
+        // public int Id { get; set; }
 
-        public byte[] PasswordHash { get; set; }
+        // public string UserName { get; set; }
 
-        public byte[] PasswordSalt { get; set; }
+        // public byte[] PasswordHash { get; set; }
+
+        // public byte[] PasswordSalt { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
@@ -28,5 +30,6 @@ namespace API.Enitities
         public ICollection<UserLike> LikedUsers { get; set; }
         public ICollection<Message> MessagesSent { get; set; }
         public ICollection<Message> MessagesReceived { get; set; }
+        public ICollection<AppUserRole> UserRoles { get; set; }
     }
 }
