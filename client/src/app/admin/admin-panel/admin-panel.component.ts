e98b766f84d4fde99a893c2e35cdea1c824d5766
Jared <jared.orr1209@gmail.com>
2021-12-18 19:31:39 -0500

    Reformatted nav bar

--- client/src/app/admin/admin-panel/admin-panel.component.ts
+++ client/src/app/admin/admin-panel/admin-panel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
   selector: 'app-admin-panel',
@@ -6,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-panel.component.css']
 })
 export class AdminPanelComponent implements OnInit {
+  user: User;
+  isAdmin: boolean;
 
-  constructor() { }
+  constructor(public accountService: AccountService) { 
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.user = user;
+      if(this.user.roles.includes("Admin"))
+        this.isAdmin = true;
+      else
+        this.isAdmin = false;
+    });
+  }
 
   ngOnInit(): void {
   }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -5,32 +5,45 @@ import { ToastrService } from 'ngx-toastr';
 import { Observable, take } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
+import {
+  faUsers,
+  faHeart,
+  faComments,
+  faUserCog,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
   user: User;
+  faUsers = faUsers;
+  faHeart = faHeart;
+  faComments = faComments;
+  faUserCog = faUserCog;
 
-  constructor(public accountService: AccountService, 
-    private router: Router, 
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
     private toastr: ToastrService,
-    private fb: FormBuilder)
-    {
-      this.accountService.currentUser$.pipe(take(1)).subscribe(user => {this.user = user});
-    }
+    private fb: FormBuilder
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe((user) => {
+      this.user = user;
+    });
+  }
 
   ngOnInit(): void {
   }
 
   login(form: NgForm) {
-    this.accountService.login(this.model).subscribe(response => {
+    this.accountService.login(this.model).subscribe((response) => {
       this.router.navigateByUrl('/members');
       form.resetForm();
-    })
+    });
   }
 
   logout() {
